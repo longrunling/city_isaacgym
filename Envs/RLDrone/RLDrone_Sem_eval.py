68abf931fe0aca007ec3e1493ef0612f55eb8902
longrunling <lrl668@hotmail.com>
2025-12-30 19:41:59 +0800

    eval env

--- Envs/RLDrone/RLDrone_Sem_eval.py
+++ Envs/RLDrone/RLDrone_Sem_eval.py
@@ -71,70 +71,7 @@ class RLDrone_Sem_eval(RLDrone_Sem):
         env_idx = self.reset_buf.nonzero(as_tuple=False).flatten()
         self.reset_idx(env_idx)
 
-        # self.render()
+        # 返回覆盖率
+        height_coverage = self.height_coverage_buf[-1]
 
-        return obs, reward, dones, self.extras
-    
-    def check_termination(self):
-        """ 
-        Check if environments need to be reset
-        """
-        # max_step
-        time_out = (self.episode_length_buf >= self.buffer_size)
-        self.reset_buf |= time_out
-        self.time_out_buf = time_out
-
-        # coverage rate
-        last_coverage_rate = self.height_coverage_buf[-1]
-        coverage = (last_coverage_rate >= self.config.height_coverage_threshold)
-        self.reset_buf |= coverage
-
-        # NOTE: add ohter termination conditions here
-
-    def calculate_reward(self):
-        '''
-        计算奖励函数
-        '''
-        # height coverage reward
-        coverage_reward = self.height_coverage_buf[-1] - self.height_coverage_buf[-2]
-        self.episode_rewards['height_coverage'] += coverage_reward
-        coverage_reward = coverage_reward * self.config.height_coverage_scale * self.dt
-
-        # termination reward
-        termination_reward = self.reset_buf * ~self.time_out_buf
-        termination_reward = termination_reward * self.config.termination_scale * self.dt
-
-        # Penalty for current episode_length
-        current_length = self.episode_length_buf.clone()
-        extra_step = -torch.clip(
-            current_length - self.config.short_path_threshold, 
-            min=0, max=self.config.max_penalty_steps)
-        self.episode_rewards['short_path'] += extra_step
-        extra_step = extra_step * self.config.short_path_scale * self.dt
-        # reward is computed cumulatively
-
-        # 记录总奖励
-        reward = coverage_reward + extra_step
-        self.episode_rewards['sum'] += reward
-
-        return reward
-    
-    def update_extra_episode_info(self, dones):
-        '''
-        用于记录结束的环境的平均奖励与平均长度
-        '''
-        new_ids = (dones > 0).nonzero(as_tuple=False)
-        for name in self.reward_keys:
-            self.rewbuffer[name].extend(self.episode_rewards[name][new_ids][:, 0].cpu().detach().numpy().tolist())
-        self.lenbuffer.extend(self.episode_length_buf[new_ids][:, 0].cpu().detach().numpy().tolist())
-
-        # 记录
-        self.extras["episode"] = {}
-        for name in self.reward_keys:
-            self.extras["episode"][f'episode_reward_{name}'] = \
-                np.mean(self.rewbuffer[name]) if len(self.rewbuffer[name]) > 0 else 0.
-        self.extras["episode"]["episode_length"] = \
-            np.mean(self.lenbuffer) if len(self.lenbuffer) > 0 else 0.
-
-    def get_privileged_observations(self):
-        pass
+        return obs, reward, dones, self.extras, height_coverage
